c896fc563a4a5d9992652c0c534c31a51c749baa
bwuzahng <bwuzhang@gmail.com>
2022-07-18 13:23:56 -0400

    Finish projects.

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,24 @@
+const HDWallet = require('truffle-hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+let secrets;
+
+if (fs.existsSync('secrets.json')) {
+ secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+}
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: new HDWallet(secrets.mnemonic, "https://rinkeby.infura.io/v3/"+secrets.infuraApiKey),
+      network_id: '4'
     }
   }
 };
